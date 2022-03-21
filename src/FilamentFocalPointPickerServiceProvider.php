42043ef6116a1a78178ccf6e8fa942c0efc5fa10
John Carter <john@johncarter.co.uk>
2022-03-21 12:11:19 +0000

    WIP

--- src/FilamentFocalPointPickerServiceProvider.php
+++ src/FilamentFocalPointPickerServiceProvider.php
@@ -2,11 +2,16 @@
 
 namespace Johncarter\FilamentFocalPointPicker;
 
+use Filament\Facades\Filament;
 use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
 
 class FilamentFocalPointPickerServiceProvider extends PluginServiceProvider
 {
+    protected array $styles = [
+        'filament-focal-point-picker' => __DIR__ . '/../resources/dist/css/filament-focal-point-picker.css',
+    ];
+
     public function configurePackage(Package $package): void
     {
         $package
